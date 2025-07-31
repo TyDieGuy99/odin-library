d376616e9fbf4628405757819f66cc356f10c751
TyDieGuy99 <120081348+TyDieGuy99@users.noreply.github.com>
2025-07-30 22:17:43 -0400

    set up DOM for user input user can input book with various values, creates div with button

--- javascript.js
+++ javascript.js
@@ -1,3 +1,11 @@
+document.getElementById('bookInfo').addEventListener('submit', function(event) {
+    event.preventDefault();
+    addBook();
+    console.log(myLibrary.length);
+    updateDisplay();
+    document.getElementById('bookInfo').reset();
+});
+
 const myLibrary = [];
 
 function Book(title, author, pages, read, id) {
@@ -8,17 +16,30 @@ function Book(title, author, pages, read, id) {
     this.id = id
 }
 
+// const book1 = new Book('Harry Potter', 'J.K. Rowling', 495, true, crypto.randomUUID());
+// const book2 = new Book('Dune', 'Some Guy', 1295, false, crypto.randomUUID());
+// const book3 = new Book('Green Eggs and Ham', 'Doctor Seus', 24, true, crypto.randomUUID());
+// const book4 = new Book('Goosebumps', 'R.L. Stein', 89, true, crypto.randomUUID());
+// const book5 = new Book('Game of Thrones', 'Forgot Name', 854, false, crypto.randomUUID());
+// myLibrary.push(book1);
+// myLibrary.push(book2);
+// myLibrary.push(book3);
+// myLibrary.push(book4);
+// myLibrary.push(book5);
+
 function addBook() {
     console.log("button has been clicked");
-    title = prompt('Title?');
-    author = prompt('Author?');
-    pages = prompt('How many pages?');
-    read = true;
+    title = document.getElementById('title').value;
+    author = document.getElementById('author').value;
+    pages = document.getElementById('pages').value;
+    if (document.getElementById('yes').checked) {
+        read = 'yes';
+    } else {
+        read = 'no';
+    }
     id = crypto.randomUUID();
     const book = new Book(title, author, pages, read, id);
     myLibrary.push(book);
-    console.log(myLibrary.length);
-    updateDisplay();
 }
 
 function updateDisplay() {
@@ -27,8 +48,11 @@ function updateDisplay() {
 
     myLibrary.forEach(obj => {
         const p = document.createElement('p');
+        const button = document.createElement('button');
         p.textContent = Object.values(obj);
+        button.textContent = 'delete the book';
         outputDiv.appendChild(p);
+        outputDiv.appendChild(button);
     })
 }
 
